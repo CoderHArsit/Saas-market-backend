61fb1a3aee4f5a171e9ddf31eb21c1602e19fd06
CoderHArsit <hs8449865663@gmail.com>
2025-09-24 01:45:41 +0530

    feat: add ci pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("heloo devops")
+console.log("I am learning ci/cd using github actions")
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("starting tests ....");
+
+setTimeout(()=> console.log('Waitin 3 seconds...'),4000)
+
+console.log("tests completed");
