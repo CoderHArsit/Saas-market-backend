dce639c0ac6d795db7cbb0eb643ca20ac3a53f40
CoderHArsit <hs8449865663@gmail.com>
2025-09-28 23:33:46 +0530

    fix: added eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.status(200).send('Hello from Saas MarketPlace')
-})
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from Saas MarketPlace');
+});
 
 export default app;
