3ce6a009180a5b75b01657e40346d8a33c62d97c
CoderHArsit <hs8449865663@gmail.com>
2025-09-28 19:48:00 +0530

    fix: fixed

--- index.js
+++
@@ -1,2 +0,0 @@
-console.log("heloo devops")
-console.log("I am learning ci/cd using github actions")
--- test.js
+++
@@ -1,5 +0,0 @@
-console.log("starting tests ....");
-
-setTimeout(()=> console.log('Waitin 3 seconds...'),4000)
-
-console.log("tests completed");
